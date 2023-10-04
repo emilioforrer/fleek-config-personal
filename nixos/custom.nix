730872a4acaf747ed735d5966911fa510df20f27
developer <dev.emilio.forrer@gmail.com>
2023-10-04 15:18:03 -0600

    fleek: apply

--- nixos/custom.nix
+++ nixos/custom.nix
@@ -1,8 +1,8 @@
-{ config, pkgs, misc, ... }: {
+{ pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
+  # List of options available: https://nix-community.github.io/home-manager/options.html
   programs.bash.bashrcExtra =  ''
      echo "Init devbox global"
      eval "$(devbox global shellenv)"
-
   '';
 }
