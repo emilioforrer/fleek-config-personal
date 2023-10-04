7dd1270cce57f7f1007a394e7cbbe747c3fa510f
developer <dev.emilio.forrer@gmail.com>
2023-10-04 14:22:17 -0600

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,7 +1,6 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
+  programs.bash.promptInit =  ''  
+    eval "$(devbox global shellenv)"
+  '';
 }
-
-programs.bash.promptInit =  ''  
-        eval "$(devbox global shellenv)"
-'';
